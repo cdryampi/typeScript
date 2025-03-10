763e6137a31923d6166811484653b43b510d55f7
cdryampi <cdryampi@gmail.com>
2025-03-10 14:41:50 +0100

    Remove unused imports, update global CSS path, and adjust Vercel adapter settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/typeScript",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
