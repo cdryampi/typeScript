74cce07696cc0b3e7da7f2526a425be76a0dd2c9
cdryampi <cdryampi@gmail.com>
2025-03-10 12:49:13 +0100

    Update base path in Astro configuration to '/typeScript'

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  base: "/",
+  base: "/typeScript",
   vite: {
     plugins: [tailwindcss()],
   },
